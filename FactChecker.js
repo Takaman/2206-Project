96be64652b1077e7dcd08247a69edd6de2a2af4e
Clarence <2102420@sit.singaporetech.edu.sg>
2023-03-04 19:10:09 +0800

    Update

--- FactChecker.js
+++ FactChecker.js
@@ -1,12 +1,28 @@
-// Define keywords for false or misleading claims
-//keywords might need to built more as fact checking websites have different ways of rating. Like four pinnochios means fake etc
 
+//keywords might need to built more as fact checking websites have different ways of rating. Like four pinnochios means fake etc
 //Define keywords for false or misleading claims. Tried my best to include all the keywords
 const falseKeywords = ["false", "misleading", "inaccurate", "unsupported", "partly false", "partly inaccurate", "Four Pinocchios",
-                      "baseless", "fabricated", "deceptive", "incorrect", "unproven", "unsubstantiated", "unfounded", "untrue", "exaggerated"];
+  "baseless", "fabricated", "deceptive", "incorrect", "unproven", "unsubstantiated", "unfounded", "untrue", "exaggerated"];
 
 // Define keywords for true or mostly true claims
-const trueKeywords = ["true", "mostly true", "correct", "accurate", "supported", "partly true", "partly accurate" ,"verifiable"];
+const trueKeywords = ["true", "mostly true", "correct", "accurate", "supported", "partly true", "partly accurate", "verifiable"];
+let rssEntries = [];
+
+//Define our RSS feeds
+const rssFeeds = [
+  "https://www.rfa.org/english/RSS",
+  "https://thediplomat.com/feed",
+  "https://e27.co/index_wp.php/feed/",
+  "https://www.asianscientist.com/feed/?x=1",
+  "http://www.asianage.com/rss_feed/",
+  "https://www.newmandala.org/feed/",
+  "https://www.asiasentinel.com/feed/",
+  "https://asia.nikkei.com/rss/feed/nar",
+  "http://www.scmp.com/rss/91/feed",
+  "https://www.channelnewsasia.com/rssfeeds/8395986",
+  "https://asean.org/feed/",
+];
+
 
 // Function to label the rating as true, false or neutral
 function labelRating(ratingText) {
@@ -29,7 +45,7 @@ function labelRating(ratingText) {
 
 // Function to check the current URL using the Chrome API
 function getCurrentUrl(callback) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const url = tabs[0].url;
     callback(url);
   });
@@ -37,15 +53,16 @@ function getCurrentUrl(callback) {
 // Handle the form submission
 function handleFormSubmit(event) {
   let selectedText = "";
-    console.log("Form submitted");
-    event.preventDefault();
-    // Get the selected text from the current tab
-    chrome.tabs.executeScript( {
-      code: "window.getSelection().toString();"
-  }, function(selection) {
-      selectedText = selection[0];   
-      
-      // Encode the query for use in the URL
+
+  console.log("Form submitted");
+  event.preventDefault();
+  // Get the selected text from the current tab
+  chrome.tabs.executeScript({
+    code: "window.getSelection().toString();"
+  }, function (selection) {
+    selectedText = selection[0];
+
+    // Encode the query for use in the URL
     console.log(selectedText);
     const query = encodeURIComponent(selectedText);
     // Define the API key and URL put in the API key and the query
@@ -60,14 +77,14 @@ function handleFormSubmit(event) {
         let queryTrueCount = 0;
         let queryFalseCount = 0;
         let queryNeutralCount = 0;
-  
+
         // Loop through each fact check result
         for (let claim of data.claims) {
           const ratingText = claim.claimReview[0].textualRating;
-  
+
           // Map the rating text to a label
           const label = labelRating(ratingText);
-  
+
           if (label === "true") {
             queryTrueCount++;
           } else if (label === "false") {
@@ -76,23 +93,23 @@ function handleFormSubmit(event) {
             queryNeutralCount++;
           }
         }
-  
+
         // Compute the weighted score for this query term
         const totalCount = queryTrueCount + queryFalseCount + queryNeutralCount;
         const queryWeightedScore = totalCount > 0 ? (queryTrueCount - queryFalseCount) / totalCount : 0;
-  
+
         // Display the results for this query term
         const resultDiv = document.getElementById("result");
         resultDiv.innerHTML = `<p>True: ${queryTrueCount}</p>
                                <p>False: ${queryFalseCount}</p>
                                <p>Neutral: ${queryNeutralCount}</p>
                                <p>Weighted score: ${queryWeightedScore}</p>`;
-  
+
         if (totalCount < 3) {
           resultDiv.innerHTML += "<p>Not enough results to make a determination</p>";
           return;
         }
-  
+
         if (queryWeightedScore < -0.5) {
           resultDiv.innerHTML += "<p>This sentence or speech is likely false</p>";
         } else if (queryWeightedScore > -0.5 && queryWeightedScore < 0) {
@@ -105,19 +122,17 @@ function handleFormSubmit(event) {
       })
       .catch(error => {
         const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = "<p>Error: Unable to fetch data from the Google Fact Check API</p>";
-        console.error(error);
+        console.log("API does not have any data. Trying Other methods. Searching of Google?")
       });
   });
 
- 
-  } 
 
-  // Add event listener 
-  document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("check_tweet_button");
-    form.addEventListener("submit", handleFormSubmit);
-  });
+}
 
-  
-  
+// Add event listener 
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("check_tweet_button");
+  form.addEventListener("submit", handleFormSubmit);
+});
+
+
