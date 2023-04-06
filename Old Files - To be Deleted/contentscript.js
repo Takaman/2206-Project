15b4479986384f180f58bdb21b774446bba9f91c
Clarence <2102420@sit.singaporetech.edu.sg>
2023-04-06 15:27:25 +0800

    Cleanup

--- Old Files - To be Deleted/contentscript.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "getSelection") {
-    sendResponse({ selection: window.getSelection().toString() });
-  }
-});
